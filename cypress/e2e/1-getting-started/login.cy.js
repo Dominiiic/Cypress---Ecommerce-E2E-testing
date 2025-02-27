aa990ec6a025ca4d2a589b312856ba29190304ce
Dominiiic <dominicocarolino15@gmail.com>
2025-02-27 20:35:57 +0800

    edited test title

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -6,7 +6,7 @@ describe('Ecommerce Login', () => {
     cy.visit('http://localhost:3000/login')
   })
 
-  it('should successfully login', () => {
+  it('should successfully login using valid credentials', () => {
     cy.get("input[name='email']").type("test@email.com")
     cy.get("input[name='password']").type("taylorslow")
     cy.get('button').contains('Sign in').click()
